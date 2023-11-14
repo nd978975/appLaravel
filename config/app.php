9bf89040d214ac4420642ed23096aeccb3f3b128
Nam Dao <nd978975@gmail.com>
2023-11-14 13:46:15 +0700

    first commit myAppLaravel

--- config/app.php
+++ config/app.php
@@ -57,9 +57,9 @@ return [
 
     'url' => env('APP_URL', 'http://localhost'),
 
-    'frontend_url' => env('FRONTEND_URL', 'https://octopus-app-faji7.ondigitalocean.app'),
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
 
-    'frontend_url' => env('FRONTEND_URL', 'https://octopus-app-faji7.ondigitalocean.app'),
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost:3000'),
 
     'asset_url' => env('ASSET_URL'),
 
