c60c63f390865cf0ec35b5cec42822a23ce4fa64
Nam Dao <nd978975@gmail.com>
2023-11-20 10:44:31 +0700

    appLaravel

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [env('FRONTEND_URL', 'https://dodungchobe.xyz')],
 
     'allowed_origins_patterns' => [],
 
